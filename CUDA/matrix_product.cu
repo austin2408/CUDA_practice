3a3cb933c589138275610f6f7bf9b462f69c7375
austin2408 <sdnd93612@gmial.com>
2022-01-25 00:32:28 +0800

    update

--- CUDA/matrix_product.cu
+++ CUDA/matrix_product.cu
@@ -24,6 +24,11 @@ __global__ void gpu_product(float* A, float* B, float* result){
 
 }
 
+// time function
+double ms_time(){
+    return (double)clock()/CLOCKS_PER_SEC*1000.0;
+}
+
 int main(){
 
     float A_m[A_row][A_col], B_m[B_row][B_col], C_m[A_row][B_col];
@@ -36,8 +41,8 @@ int main(){
 	{
 		for (int j = 0; j < A_col; j++)
 		{
-			// A_m[i][j] = (float)rand()/RAND_MAX*2-1;
-            A_m[i][j] = i;
+			A_m[i][j] = (float)rand()/RAND_MAX*2-1;
+            // A_m[i][j] = i;
 			printf("%f   ", (float)A_m[i][j]);
 		}
 		printf("\n");
@@ -50,8 +55,8 @@ int main(){
 	{
 		for (int j = 0; j < B_col; j++)
 		{
-			// B_m[i][j] = (float)rand()/RAND_MAX*2-1;
-            B_m[i][j] = j;
+			B_m[i][j] = (float)rand()/RAND_MAX*2-1;
+            // B_m[i][j] = j;
 			printf("%f   ", (float)B_m[i][j]);
 		}
 		printf("\n");
@@ -70,7 +75,9 @@ int main(){
     dim3 numBlock(A_row, B_col, 1);
 
     // implement kernel
+    double t0 = ms_time();
     gpu_product<<<1, numBlock>>>(A, B, C);
+    double device_time = ms_time() - t0;
 
     cudaDeviceSynchronize();
 
@@ -87,6 +94,7 @@ int main(){
 		}
 		printf("\n");
 	}
+    printf("Time of gpu : %g\n", device_time);
 
     cudaFree(A);
     cudaFree(B);
