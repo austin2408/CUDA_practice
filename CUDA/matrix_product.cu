7fb963db4201f401b55fd8ba17565c098b246f82
austin2408 <sdnd93612@gmial.com>
2022-01-25 00:15:04 +0800

    update

--- CUDA/matrix_product.cu
+++ CUDA/matrix_product.cu
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <cuda.h>
+#include<iostream>
 #include <cuda_runtime.h>
+#include <math.h>
 
 #define A_row 5
 #define A_col 2
 #define B_row 2
 #define B_col 4
 
-__global__ void gpu_product(int *A, int *B, int *C){
+__global__ void gpu_product(float* A, float* B, float* result){
     // 0~4
     int row = threadIdx.x;
 
@@ -15,68 +17,76 @@ __global__ void gpu_product(int *A, int *B, int *C){
     int col = threadIdx.y;
 
     for (int i=0; i<A_col; i++){
-        C[i*B_col + col] += A[row*A_col + i]*B[B_col + col];
+        // printf("row : %d || col : %d || i : %d\n",row*B_col + col, A[row*A_col + i], B[i*B_col + col]);
+
+        result[row*B_col + col] += A[row*A_col + i]*B[i*B_col + col];
     }
+
 }
 
 int main(){
 
-    int A_m[A_row][A_col], B_m[B_row][B_col], C_m[A_col][B_row];
+    float A_m[A_row][A_col], B_m[B_row][B_col], C_m[A_row][B_col];
 
-    int *A, *B, *C;
+    float* A, * B, * C;
 
+    // set A matrix
     printf("a Matrix: \n");
 	for (int i = 0; i < A_row; i++)
 	{
 		for (int j = 0; j < A_col; j++)
 		{
-			A_m[i][j] = i;
-			printf("%d   ", (int)A_m[i][j]);
+			// A_m[i][j] = (float)rand()/RAND_MAX*2-1;
+            A_m[i][j] = i;
+			printf("%f   ", (float)A_m[i][j]);
 		}
 		printf("\n");
 	}
+    printf("\n");
 
+    // Set B matrix
     printf("b Matrix: \n");
 	for (int i = 0; i < B_row; i++)
 	{
 		for (int j = 0; j < B_col; j++)
 		{
-			B_m[i][j] = i;
-			printf("%d   ", (int)B_m[i][j]);
+			// B_m[i][j] = (float)rand()/RAND_MAX*2-1;
+            B_m[i][j] = j;
+			printf("%f   ", (float)B_m[i][j]);
 		}
 		printf("\n");
 	}
+    printf("\n");
 
-    cudaMalloc((void **)&A, A_row * A_col * sizeof(int));
-    cudaMalloc((void **)&B, B_row * B_col * sizeof(int));
-    cudaMalloc((void **)&C, A_col * B_row * sizeof(int));
+    // Define device's (GPU) memory
+    cudaMalloc((void **)&A, A_row * A_col * sizeof(float));
+    cudaMalloc((void **)&B, B_row * B_col * sizeof(float));
+    cudaMalloc((void **)&C, A_row * B_col * sizeof(float));
 
-    cudaMemcpy(A, A_m, A_row * A_col * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(B, B_m, B_row * B_col * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(C, C_m, A_col * B_row * sizeof(int), cudaMemcpyHostToDevice);
+    // pass data to device
+    cudaMemcpy(A, A_m, A_row * A_col * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(B, B_m, B_row * B_col * sizeof(float), cudaMemcpyHostToDevice);
 
-    dim3 numBlock(A_row, B_col);
+    dim3 numBlock(A_row, B_col, 1);
 
+    // implement kernel
     gpu_product<<<1, numBlock>>>(A, B, C);
 
     cudaDeviceSynchronize();
 
-    cudaMemcpy(C_m, C, A_col * B_row * sizeof(int), cudaMemcpyDeviceToHost);
+    // pass data to host
+    cudaMemcpy(C_m, C, A_row * B_col * sizeof(float), cudaMemcpyDeviceToHost);
 
+    // Show result
     printf("c Matrix: \n");
 	for (int i = 0; i < A_row; i++)
 	{
 		for (int j = 0; j < B_col; j++)
 		{
-			A_m[i][j] = i;
-			printf("%d   ", (int)C_m[i][j]);
+			printf("%f   ", (float)C_m[i][j]);
 		}
 		printf("\n");
 	}
-
-    free(A_m);
-    free(B_m);
-    free(C_m);
 
     cudaFree(A);
     cudaFree(B);
